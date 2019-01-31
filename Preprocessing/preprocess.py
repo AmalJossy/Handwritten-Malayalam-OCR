47fd436c76ec699235a10f1bcc65279cc207082e
Amal Jossy <amaljossy1@gmail.com>
2019-01-31 11:27:16 +0530

    change input directory

--- Preprocessing/preprocess.py
+++ Preprocessing/preprocess.py
@@ -4,7 +4,7 @@ import numpy as np
 def preprocess():
     print("Denoising")
     #import image
-    path=os.path.join(os.getcwd(),'working')
+    path=os.path.join(os.getcwd(),'working/input')
     inputfile=os.listdir(path)[0]
     image = cv2.imread(path+'/'+inputfile)
     # cv2.imshow('input',image)
