f37b182e6ea61d5e563c0071778996b11b4ccee0
Amal Jossy <amaljossy1@gmail.com>
2019-01-31 15:57:16 +0530

    denoising

--- Preprocessing/preprocess.py
+++ Preprocessing/preprocess.py
@@ -6,7 +6,15 @@ def preprocess():
     #import image
     path=os.path.join(os.getcwd(),'working/input')
     inputfile=os.listdir(path)[0]
-    image = cv2.imread(path+'/'+inputfile)
-    # cv2.imshow('input',image)
-    # cv2.waitKey(0)
-    cv2.imwrite('working/processed.jpg',image)
+    image = cv2.imread(path+'/'+inputfile,0)
+    cv2.imshow('input',255-image)
+    cv2.waitKey(0)
+    blur = cv2.GaussianBlur(image,(5,5),0)
+    ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)    
+    # kernel = np.ones((5,5),np.uint8) // try different  kernal size
+    # opening = cv2.morphologyEx(th3, cv2.MORPH_OPEN, kernel)
+    mask=255-th3
+    denoised=255-(image * (mask.astype(image.dtype)))
+    cv2.imshow('denoise',denoised)
+    cv2.waitKey(0)
+    cv2.imwrite('working/processed.jpg',denoised)
--- Segmentation/segment.py
+++ Segmentation/segment.py
@@ -8,6 +8,6 @@ def segment():
     # inputfile=os.listdir(path)[0]
     inputfile='processed.jpg'
     # print(inputfile)
-    image = cv2.imread(path+'/'+inputfile)
+    image = cv2.imread(path+'/'+inputfile,0)
     cv2.imshow('orig',image)
     cv2.waitKey(0)
