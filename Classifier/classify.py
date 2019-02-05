86812b1f2aba4e0d18cd76782918c82f7916019d
Amal Jossy <amaljossy1@gmail.com>
2019-02-05 15:07:40 +0530

    segmentation started

--- Classifier/classify.py
+++ Classifier/classify.py
@@ -1,3 +1,6 @@
 
 def getLabels():
+    # read files of working/words/<word folder>/<char images
+    # classify
+    # append to 2D list
     return [[1,2],[1,2,3]]
--- Preprocessing/preprocess.py
+++ Preprocessing/preprocess.py
@@ -1,13 +1,14 @@
 import cv2
 import os
 import numpy as np
+
 def preprocess():
     print("Denoising")
     #import image
     path=os.path.join(os.getcwd(),'working/input')
     inputfile=os.listdir(path)[0]
     image = cv2.imread(path+'/'+inputfile,0)
-    cv2.imshow('input',255-image)
+    cv2.imshow('input',image)
     cv2.waitKey(0)
     blur = cv2.GaussianBlur(image,(5,5),0)
     ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)    
@@ -15,6 +16,8 @@ def preprocess():
     # opening = cv2.morphologyEx(th3, cv2.MORPH_OPEN, kernel)
     mask=255-th3
     denoised=255-(image * (mask.astype(image.dtype)))
-    cv2.imshow('denoise',denoised)
-    cv2.waitKey(0)
+    # cv2.imshow('denoise',denoised)
+    # cv2.imshow('inverse binary',mask)
+    # cv2.waitKey(0)
+    cv2.imwrite('working/invbinary.jpg',mask)
     cv2.imwrite('working/processed.jpg',denoised)
