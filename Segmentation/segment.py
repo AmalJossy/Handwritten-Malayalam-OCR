d3064650858436bd61774a52d61ded577c49c38f
hashrin <ashucp@gmail.com>
2019-06-13 14:54:17 +0530

    final commit

--- Segmentation/segment.py
+++ Segmentation/segment.py
@@ -7,8 +7,30 @@ def segment():
     #import image
     path=os.path.join(os.getcwd(),'working/')
     # inputfile=os.listdir(path)[0]
-    inputfile='shadowresult.jpg'
-    gray = cv2.imread(path+inputfile,0)
+    inputfile=cv2.imread(path+'shadowtest.jpg',0)
+
+    #SHADOW REMOVAL
+
+    rgb_planes=cv2.split(inputfile)
+    result_planes=[]
+    result_norm_planes=[]
+    kernel = np.ones((7,7),np.uint8)
+    for plane in rgb_planes:
+        dilated_img=cv2.dilate(plane,kernel)
+        bg_img=cv2.medianBlur(dilated_img,21) #scratch text out
+        diff_img=255-cv2.absdiff(plane,bg_img)
+        norm_img=cv2.normalize(diff_img,diff_img,alpha=0,beta=255,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8UC1)
+        result_planes.append(diff_img)
+        result_norm_planes.append(norm_img)
+    result=cv2.merge(result_norm_planes)
+    cv2.imwrite(path+'shadow_removed.jpg',result)
+
+
+
+
+
+
+    gray = cv2.imread(path+'shadow_removed.jpg',0)
     blur = cv2.GaussianBlur(gray,(5,5),0)
     ret,line_binary = cv2.threshold(blur,127,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     line_binary=255-line_binary
